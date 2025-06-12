562de030e5f50e9ecf8a4214d8cb58ffe48e0b52
Diego Gonzalez <diegoandresgonzalez.dag@gmail.com>
2025-06-12 14:44:04 -0300

    change bpmInput to accept keyboard

--- src/components/metronome/components/bpmInput.tsx
+++ src/components/metronome/components/bpmInput.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { MAX_BPM, MIN_BPM } from "../../../utils/constants";
 
 type Props = {
@@ -12,24 +13,44 @@ const BPMInput = (props: Props) => {
     handleChange,
   } = props;
 
+  const [auxBPM, setAuxBPM] = useState(String(value));
+
+  const handleSubmit = (newValue = auxBPM) => {
+    let valueToSubmit = parseInt(newValue);
+    if (!valueToSubmit || isNaN(valueToSubmit) || valueToSubmit < MIN_BPM || valueToSubmit > MAX_BPM) {
+      setAuxBPM(String(value));
+      return;
+    }
+
+    setAuxBPM(String(valueToSubmit));
+    handleChange(valueToSubmit);
+  }
+
   return (
     <div className="bpmInputContainer">
-      <p
+      <input
+        type="number"
         className="bpmInput"
-        onWheel={(e) => {
-          const valueToAdd = e.deltaY * -0.01;
-          handleChange(value + valueToAdd)
+        min={MIN_BPM}
+        max={MAX_BPM}
+        value={auxBPM}
+        onChange={(e) => setAuxBPM(e.target.value.substring(0, 3))}
+        onMouseEnter={(e) => e.currentTarget.focus()}
+        onMouseLeave={(e) => e.currentTarget.blur()}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") {
+            handleSubmit();
+          }
         }}
-      >
-        {value}
-        </p>
+        onBlur={() => handleSubmit()}
+      />
       <input
         type="range"
         className="bpmInputRange"
         min={MIN_BPM}
         max={MAX_BPM}
         value={value}
-        onChange={(e) => handleChange(parseInt(e.target.value))}
+        onChange={(e) => handleSubmit(e.target.value)}
       />
     </div>
   );
