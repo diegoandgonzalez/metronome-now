ce8d3447d2afc36a2a7fc7fe89ba9f351512cfdf
Diego Gonzalez <diegoandresgonzalez.dag@gmail.com>
2025-06-12 21:31:14 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app";
+import App from "./App";
 import "./i18n/index";
 import "./styles/index.css";
 
