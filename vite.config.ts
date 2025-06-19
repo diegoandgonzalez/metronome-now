90b225774c59ac7ba2e6c8a13b5869eed193bfa0
Diego Gonzalez <diegoandresgonzalez.dag@gmail.com>
2025-06-19 12:13:03 -0300

    add manifest and pwa plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+import { manifestForPlugIn } from "./manifest";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugIn)],
   server: {
     open: true,
     port: 3000,
