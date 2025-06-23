2fe5111bb88587157bc04c95bce0d126b233b35d
Diego Gonzalez <diegoandresgonzalez.dag@gmail.com>
2025-06-23 11:44:42 -0300

    beatDisplay refactor and visual update

--- src/components/metronome/components/beatDisplay.tsx
+++ src/components/metronome/components/beatDisplay.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from "react";
 import { useTranslation } from "react-i18next";
 
 type Props = {
@@ -18,26 +19,44 @@ const BeatDisplay = (props: Props) => {
 
   const { t } = useTranslation();
 
+  const splitBeatArray = useMemo(() => {
+    const spliceIndex = beatsPerMeasure <= 4 ? beatsPerMeasure : Math.floor(beatsPerMeasure / 2);
+
+    const beatArray = Array.from(Array(beatsPerMeasure).keys());
+    const firstHalfArray = beatArray.splice(0, spliceIndex);
+    const secondHalfArray = beatArray;
+
+    return [firstHalfArray, secondHalfArray];
+  }, [beatsPerMeasure]);
+
   return (
     <div className="beatContainer">
       {
-        [...Array(beatsPerMeasure)].map((_, beatIndex) => {
-
-          const isCurrentBeat = currentBeatInMeasure === beatIndex;
-          const beatType = beatTypes[beatIndex];
-
+        splitBeatArray.map((beatArray, beatArrayIndex) => {
           return (
-            <button
-              key={beatIndex}
-              className="beat"
-              title={t("clickToToggleBeatType")}
-              data-beat-type={String(beatType)}
-              data-is-current-beat={String(isCurrentBeat)}
-              onClick={(e) => {
-                handleClick(beatIndex);
-                e.currentTarget.blur();
-              }}
-            />
+            <div key={beatArrayIndex}>
+              {
+                beatArray.map((beatIndex) => {
+
+                  const isCurrentBeat = currentBeatInMeasure === beatIndex;
+                  const beatType = beatTypes[beatIndex];
+
+                  return (
+                    <button
+                      key={beatIndex}
+                      className="beat"
+                      data-beat-type={String(beatType)}
+                      data-is-current-beat={String(isCurrentBeat)}
+                      title={t("clickToToggleBeatType")}
+                      onClick={(e) => {
+                        handleClick(beatIndex);
+                        e.currentTarget.blur();
+                      }}
+                    />
+                  )
+                })
+              }
+            </div>
           )
         })
       }
