87ac2fb0326ab3046e97fb33f6806b35ec37590c
Diego Gonzalez <diegoandresgonzalez.dag@gmail.com>
2025-10-07 14:31:07 -0300

    add autoFocus in tempoProgrammingTimer dialog

--- src/components/metronome/components/dialogs/tempoProgrammingTimerDialog.tsx
+++ src/components/metronome/components/dialogs/tempoProgrammingTimerDialog.tsx
@@ -139,6 +139,7 @@ const TempoProgrammingTimerDialog = (props: Props) => {
             <h4>{t("bpmProgramming")}</h4>
             <label>
                 <input
+                    autoFocus
                     id="tempoProgrammingIsActive"
                     type="checkbox"
                     checked={isActive}
