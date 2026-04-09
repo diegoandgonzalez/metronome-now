27000b47cf473f693c1eb78217244ffbfe611010
Diego Gonzalez <diegoandresgonzalez.dag@gmail.com>
2026-04-09 16:59:16 -0300

    fix metadata

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import { Helmet, HelmetProvider } from "react-helmet-async";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import SnackbarContext from "./components/snackbar/snackbarContext"
@@ -6,32 +5,19 @@ import useSnackbar from "./components/snackbar/useSnackbar"
 import Metronome from "./components/metronome"
 import Snackbar from "./components/snackbar"
 import appTheme from "./styles/theme";
-import { useTranslation } from "react-i18next";
 
 const App = () => {
 
   const snackbarValue = useSnackbar();
-  const { t } = useTranslation();
 
   return (
-    <HelmetProvider>
       <SnackbarContext value={snackbarValue}>
         <ThemeProvider theme={appTheme}>
-          <Helmet>
-            <meta name="description" content={t("meta.description")} />
-            <meta property="og:description" content={t("meta.description")} />
-            <meta property="og:title" content={"Metronome Now"} />
-            <meta property="og:type" content="website" />
-            <meta name="twitter:description" content={t("meta.description")} />
-            <meta name="twitter:title" content={"Metronome Now"} />
-            <meta name="twitter:card" content="summary" />
-          </Helmet>
           <CssBaseline />
           <Metronome />
         </ThemeProvider>
         <Snackbar />
       </SnackbarContext>
-    </HelmetProvider>
   )
 }
 
